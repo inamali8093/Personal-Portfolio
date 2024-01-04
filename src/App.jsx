60556975d5b4dd3ab2573a61e1d859bb63e2afb9
Inam <inamali0291@gmail.com>
2024-01-04 08:30:44 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Skills from './pages/Skills'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/contact'
 import { Outlet } from 'react-router-dom'
 
 function App() {
